6f5370f715b43a197685110d8866502da013bb59
Emett Speer <speer.emett@gmail.com>
2015-08-26 00:23:53 -0700

    Added more test

--- tests/test_base_api.py
+++ tests/test_base_api.py
@@ -2,13 +2,24 @@ import unittest
 import requests_mock
 import json
 import os
+import requests
 
 
 from pyem7.base_api import BaseAPI
+from pyem7.custom_errors import AuthError
+
+
+with open(os.path.join(os.path.dirname(__file__), 'data/base_api.json')) as data_file:
+    data = json.load(data_file)
+data_file.closed
 
 
 @requests_mock.mock()
 class TestSetConnection(unittest.TestCase):
+    with open(os.path.join(os.path.dirname(__file__), 'data/base_api.json')) as data_file:
+        data = json.load(data_file)
+    data_file.closed
+
     def setUp(self):
         self.base_api = BaseAPI()
         self.base_api.set_connection(
@@ -31,11 +42,9 @@ class TestSetConnection(unittest.TestCase):
 
 @requests_mock.mock()
 class TestPerformRequest(unittest.TestCase):
-    with open(os.path.join(
-            os.path.dirname(__file__),
-            'data/base_api.json'
-    )) as data_file:
+    with open(os.path.join(os.path.dirname(__file__), 'data/base_api.json')) as data_file:
         data = json.load(data_file)
+    data_file.closed
 
     def setUp(self):
         self.base_api = BaseAPI()
@@ -46,11 +55,124 @@ class TestPerformRequest(unittest.TestCase):
             session_verify=False
         )
 
-    def test__perform_request_with_get(self, mock):
+    def test__perform_request_with_no_user_name(self, mock):
+        headers = self.data['auth-error-header']
+        response_data = self.data['auth-error-content']
+        self.base_api.set_connection(
+            user_name='',
+            password="test_password",
+            end_point="mock:://api.test.com",
+            session_verify=False
+        )
+        mock.get(requests_mock.ANY, json=response_data, headers=headers)
+
+        with self.assertRaises(AuthError):
+            self.base_api._perform_request(self.base_api.end_point)
+
+    def test__perform_request_with_with_none_as_user_name(self, mock):
+        headers = self.data['auth-error-header']
+        response_data = self.data['auth-error-content']
+        self.base_api.set_connection(
+            user_name=None,
+            password="test_password",
+            end_point="mock:://api.test.com",
+            session_verify=False
+        )
+        mock.get(requests_mock.ANY, json=response_data, headers=headers)
+
+        with self.assertRaises(AuthError):
+            self.base_api._perform_request(self.base_api.end_point)
+
+    def test__perform_request_with_no_password_name(self, mock):
+        headers = self.data['auth-error-header']
+        response_data = self.data['auth-error-content']
+        self.base_api.set_connection(
+            user_name='test',
+            password="",
+            end_point="mock:://api.test.com",
+            session_verify=False
+        )
+        mock.get(requests_mock.ANY, json=response_data, headers=headers)
+
+        with self.assertRaises(AuthError):
+            self.base_api._perform_request(self.base_api.end_point)
+
+    def test__perform_request_with_none_as_password_name(self, mock):
+        headers = self.data['auth-error-header']
+        response_data = self.data['auth-error-content']
+        self.base_api.set_connection(
+            user_name='test',
+            password=None,
+            end_point="mock:://api.test.com",
+            session_verify=False
+        )
+        mock.get(requests_mock.ANY, json=response_data, headers=headers)
+
+        with self.assertRaises(AuthError):
+            self.base_api._perform_request(self.base_api.end_point)
+
+    def test__perform_request_returns_requests_object(self, mock):
         headers = self.data['good-header']
         response_data = self.data['good-content']
         mock.get(requests_mock.ANY, json=response_data, headers=headers)
 
         test = self.base_api._perform_request(self.base_api.end_point)
 
-        self.assertEqual(test.headers['content-type'], 'application/json')
+        self.assertIsInstance(test, requests.models.Response)
+
+
+@requests_mock.mock()
+class TestPost(unittest.TestCase):
+    pass
+
+
+@requests_mock.mock()
+class TestDelete(unittest.TestCase):
+    pass
+
+
+@requests_mock.mock()
+class TestGet(unittest.TestCase):
+    pass
+
+
+@requests_mock.mock()
+class TestFind(unittest.TestCase):
+    pass
+
+
+@requests_mock.mock()
+class TestGetURI(unittest.TestCase):
+    with open(os.path.join(
+            os.path.dirname(__file__),
+            'data/base_api.json'
+    )) as data_file:
+        data = json.load(data_file)
+    data_file.closed
+
+    def setUp(self):
+        self.base_api = BaseAPI()
+        self.base_api.set_connection(
+            user_name="test_user",
+            password="test_password",
+            end_point="mock:://api.test.com",
+            session_verify=False
+        )
+
+    def test_gest_uri_with_good_data(self, mock):
+        pass
+
+
+@requests_mock.mock()
+class TestPayload(unittest.TestCase):
+    pass
+
+
+@requests_mock.mock()
+class TestCreate(unittest.TestCase):
+    pass
+
+
+@requests_mock.mock()
+class TestUpdate(unittest.TestCase):
+    pass
